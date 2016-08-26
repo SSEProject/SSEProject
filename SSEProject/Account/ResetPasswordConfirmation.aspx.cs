309962490289b394b8d71510446dbaef086532de
Matthew <Matthew@MATTMO>
2016-08-26 13:27:21 -0400

    Add project files.

---
+++ SSEProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SSEProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SSEProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SSEProject.Startup))]
+namespace SSEProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
