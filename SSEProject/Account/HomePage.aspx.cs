f77fa95c41e2f898f6bc7c990b09afdbddb8a472
Noama Samreen <samreen@etsu.edu>
2016-09-11 01:44:28 -0400

    Tested To-do List with local Access DB

--- SSEProject/Account/HomePage.aspx.cs
+++ SSEProject/Account/HomePage.aspx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Diagnostics;
 
 namespace SSEProject.Account
 {
@@ -17,6 +14,23 @@ namespace SSEProject.Account
         protected void itemsGrid_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        protected void GetList_Click(object sender, EventArgs e)
+        {
+            String name = "Items";
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\Resources\Sample.xlsx;
+                                        Extended Properties='Excel 8.0;HDR=Yes;IMEX=1'";
+
+
+            OleDbConnection con = new OleDbConnection(connectionString);
+            OleDbCommand oconn = new OleDbCommand("Select * From [" + name + "$]", con);
+            con.Open();
+        
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            itemsGrid.DataSource = data;
         }
     }
 }
--- SSEProject/Account/HomePage.aspx.designer.cs
+++ SSEProject/Account/HomePage.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace SSEProject.Account {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView itemsGrid;
+        
+        /// <summary>
+        /// SqlDataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource;
     }
 }
